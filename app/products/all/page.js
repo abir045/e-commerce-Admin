f818db2a7c7291acacce14481e06e72fbf08f0d2
Salekin Imran <abir045@gmail.com>
2023-07-06 17:51:02 +0600

    added products in product page

---
+++ app/products/all/page.js
@@ -0,0 +1,29 @@
+import React from "react";
+import axios from "axios";
+import Products from "@/app/components/Products";
+
+async function fetchProducts() {
+  const response = await fetch("https://fakestoreapi.com/products?limit=10");
+
+  await new Promise((resolve) => setTimeout(resolve, 1000));
+
+  const products = await response.json();
+
+  return products;
+}
+
+const ProductPage = async () => {
+  const products = await fetchProducts();
+  console.log(products);
+
+  return (
+    <div className="flex mx-[10%]">
+      {/* {products.map((item) => (
+        <div>{item.title}</div>
+      ))} */}
+      <Products products={products} />
+    </div>
+  );
+};
+
+export default ProductPage;
